44f105afab84fec2a26fe132823842aefa358aaf
Jarod Christiansen <jarodchristiansen2@gmail.com>
2021-12-19 08:11:03 -0700

    updating favorites component

--- components/profile/favorites.js
+++ components/profile/favorites.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { getSession } from "next-auth/client";
 import fetch from "unfetch";
 import { useRouter } from "next/router";
+import { Accordion } from "react-bootstrap";
 
 function Favorites() {
   let fetchedUser;
@@ -11,7 +12,6 @@ function Favorites() {
   const [loadedSession, setLoadedSession] = useState();
   const [loadedUser, setLoadedUser] = useState();
   const [favorites, setFavorites] = useState();
-
 
   useEffect(() => {
     getSession().then((session) => {
@@ -32,47 +32,50 @@ function Favorites() {
     setLoadedUser(fetchedUser);
 
     if (fetchedUser) {
-        setFavorites(fetchedUser?.favorites)
+      setFavorites(fetchedUser?.favorites);
     }
     console.log("this is fetchedUser", loadedUser);
   };
 
   return (
-    <div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            textAlign: "center",
-            width: "50%",
-            border: "2px solid black",
-            marginLeft: "25%",
-          }}
-        >
-          Favorites Panel
-          {loadedSession?.user?.favorites?.length > 1 && loadedSession?.user?.favorites.map((y) => {
-            return (
-              <div key={y.title}>
-                {y.title}
-                {y.symbol}
-                <img src={y.image} />
-              </div>
-            );
-          })}
-
-            {loadedUser?.favorites?.length >= 1 && loadedUser?.favorites?.map((y) => {
+    <div style={{ width: "80%", border: "2px solid black", marginLeft: "10%" }}>
+      <Accordion defaultActiveKey="0" flush={true}>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header className={"accordion-flush"}>
+            Favorites
+          </Accordion.Header>
+          <Accordion.Body
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              textAlign: "center",
+            }}
+          >
+            {loadedSession?.user?.favorites?.length > 1 &&
+              loadedSession?.user?.favorites.map((y) => {
                 return (
-                    <div key={y.title}>
-                        {y.title}
-                        {y.symbol}
-                        <img src={y.image} />
-                    </div>
+                  <div key={y.title}>
+                    {y.title}
+                    {y.symbol}
+                    <img src={y.image} />
+                  </div>
                 );
-            })}
+              })}
 
-        </div>
+            {loadedUser?.favorites?.length >= 1 &&
+              loadedUser?.favorites?.map((y) => {
+                return (
+                  <div key={y.title}>
+                    {y.title}
+                    {y.symbol}
+                    <img src={y.image} />
+                  </div>
+                );
+              })}
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
     </div>
   );
 }
