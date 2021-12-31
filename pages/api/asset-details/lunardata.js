7d81d6bf2d0f19d394f29b41023007a45794c2a7
Jarod Christiansen <jarodchristiansen2@gmail.com>
2021-12-31 14:24:37 -0700

    Added memory-cache for lunarcrush, needs further testing

--- pages/api/asset-details/lunardata.js
+++ pages/api/asset-details/lunardata.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+var cache = require("memory-cache");
 
 export default async (req, res) => {
   // `https://api.lunarcrush.com/v2?data=assets&key=${key}&symbol=${id}&data_points=${time}&interval=day`,
@@ -7,11 +8,23 @@ export default async (req, res) => {
   let id = req.query.symbol;
   let time = parseInt(req.query.time);
 
-  let data = await fetch(
-    `https://api.lunarcrush.com/v2?data=assets&key=${key}&symbol=${id}&data_points=${time}&interval=day`
-  ).then((response) => response.json());
+  let testData = cache.get(`lunarData: ${id}`);
 
-  console.log("lunardata endpoint hit yyeeeeeeeet", data);
+  let data;
+  if (testData) {
+    data = testData;
+    console.log("testDta is there, setting from cache");
+  } else {
+    data = await fetch(
+      `https://api.lunarcrush.com/v2?data=assets&key=${key}&symbol=${id}&data_points=${time}&interval=day`
+    ).then((response) => response.json());
+
+    //Hits this area if the value is no longer in the cache to prevent the data from being hit too frequently. currently 12 hours as it is on a daily UI
+    cache.put(`lunarData: ${id}`, data, 43200000, function (key, value) {
+      console.log(key + " did " + value);
+    });
+  }
+
   res.json({ data: data });
 
   // if (assetCollection) {
