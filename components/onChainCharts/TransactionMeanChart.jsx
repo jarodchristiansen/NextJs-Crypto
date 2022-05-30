90979735604ba639d3f256b5d701453a638e2256
Jarod Christiansen <jarodchristiansen2@gmail.com>
2022-05-29 20:15:59 -0700

    adding glassnode indicators

---
+++ components/onChainCharts/TransactionMeanChart.jsx
@@ -0,0 +1,50 @@
+import {
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  LineChart,
+  Line,
+} from "recharts";
+import { currencyFormat } from "../../helpers/formatters";
+import { useMediaQuery } from "react-responsive";
+
+const TransactionMeanChart = ({ data }) => {
+  const isMobile = useMediaQuery({
+    query: `(max-width: 920px)`,
+  });
+  return (
+    <div>
+      <h1>Transaction Size Mean Chart</h1>
+      {console.log("transaction data chart", data)}
+      {data && (
+        <ResponsiveContainer height={350}>
+          <LineChart
+            width={430}
+            height={250}
+            data={data}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+
+            {isMobile ? (
+              <XAxis dataKey="t" height={0} />
+            ) : (
+              <XAxis dataKey="t" />
+            )}
+
+            {!isMobile && <YAxis dataKey={"v"} />}
+
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="v" stroke="#8884d8" />
+          </LineChart>
+        </ResponsiveContainer>
+      )}
+    </div>
+  );
+};
+
+export default TransactionMeanChart;
--- pages/api/indicators/indicators.jsx
+++ pages/api/indicators/indicators.jsx
@@ -26,25 +26,23 @@ export default async (req, res) => {
     // data = await fetch(
     //   `https://api.glassnode.com/v1/metrics/transactions/count?a=${id}&api_key=${GLASSNODE_KEY}`
     // ).then((response) => response.json());
-
     await Promise.all([
       fetch(
-        `https://api.glassnode.com/v1/metrics/transactions/count?a=${id}&api_key=${GLASSNODE_KEY}`
+        `https://api.glassnode.com/v1/metrics/indicators/difficulty_ribbon?a=${id}&api_key=${GLASSNODE_KEY}`
       ).then((resp) => resp.json()),
       fetch(
-        `https://api.glassnode.com/v1/metrics/transactions/rate?a=${id}&api_key=${GLASSNODE_KEY}`
+        `https://api.glassnode.com/v1/metrics/indicators/sopr?a=${id}&api_key=${GLASSNODE_KEY}`
       ).then((resp) => resp.json()),
-      fetch(
-        `https://api.glassnode.com/v1/metrics/transactions/size_mean?a=${id}&api_key=${GLASSNODE_KEY}`
-      ).then((resp) => resp.json()),
-      fetch(
-        `https://api.glassnode.com/v1/metrics/transactions/size_sum?a=${id}&api_key=${GLASSNODE_KEY}`
-      ).then((resp) => resp.json()),
+      // fetch(
+      //     `https://api.glassnode.com/v1/metrics/transactions/size_mean?a=${id}&api_key=${GLASSNODE_KEY}`
+      // ).then((resp) => resp.json()),
+      // fetch(
+      //     `https://api.glassnode.com/v1/metrics/transactions/size_sum?a=${id}&api_key=${GLASSNODE_KEY}`
+      // ).then((resp) => resp.json()),
     ]).then((result) => {
-      console.log("this is the result", result);
+      console.log("this is the result in indicators", result);
       data = result;
     });
-
     // Hits this area if the value is no longer in the cache to prevent the data from being hit too frequently. currently 12 hours as it is on a daily UI
     // cache.put(
     //   `Data: assetAnalytics:${id} - requestType:${requestType}`,
